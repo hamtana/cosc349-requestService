9032ad9c760414295d83f9401ee4a34e39d57d07
phiha428 <phiha428@student.otago.ac.nz>
2024-08-23 11:24:25 +1200

    added interface for requestDAO, references #3

---
+++ register-service/src/main/java/dao/RegisterDAO.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import domain.Request;
+import java.util.List;
+
+/**
+ *
+ * @author hamishp
+ */
+public interface RegisterDAO {
+
+    List<Request> getAllRequests();
+
+    Request getRequestById(String id);
+
+    void createRequest(Request request);
+
+    void updateRequest(Request request);
+
+    void delete(String id);
+
+}
