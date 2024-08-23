c139c1a5e6205d64f493acf2a252f3cc2cc5159c
phiha428 <phiha428@student.otago.ac.nz>
2024-08-23 21:41:37 +1200

    completed requestCollectionsDAO, now need to test, closes #4

---
+++ register-service/src/test/java/dao/RequestCollectionsDAOTest.java
@@ -0,0 +1,38 @@
+package dao;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RequestCollectionsDAOTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAllRequests() {
+    }
+
+    @Test
+    void getRequestById() {
+    }
+
+    @Test
+    void createRequest() {
+    }
+
+    @Test
+    void updateRequest() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
