e89bef70747fec04ecedc06864c57aeabfaa2cac
phiha428 <phiha428@student.otago.ac.nz>
2024-08-23 12:02:45 +1200

    added some methods to RequestCollectionDAO, references #4

---
+++ register-service/src/main/java/dao/RequestCollectionsDAO.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import domain.Request;
+import java.util.HashMap;
+
+/**
+ *
+ * @author hamishp
+ */
+public class RequestCollectionsDAO implements RequestDAO {
+
+    private static final Multimap<String, Request> requests = HashMultimap.create();
+    private static final Multimap<String, Request> requestsByTenant = HashMultimap.create();
+
+    @Override
+    public void createRequest(Request request){
+        requests.put(request.getId(), request);
+        requestsByTenant.put(request.getTenant().getId(), request);
+    }
+    
+    @Override
+    public void 
+
+}
--- register-service/src/main/java/dao/RequestDAO.java
+++ register-service/src/main/java/dao/RequestDAO.java
@@ -11,7 +11,7 @@ import java.util.List;
  *
  * @author hamishp
  */
-public interface RegisterDAO {
+public interface RequestDAO {
 
     List<Request> getAllRequests();
 
