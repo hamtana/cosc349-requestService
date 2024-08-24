14dd7bc2dcb2926aaabb2ab820eb1279331d17b1
phiha428 <phiha428@student.otago.ac.nz>
2024-08-24 12:32:04 +1200

    added add-tenant js, references #6

---
+++ register-service/src/static/js/add-tenant.js
@@ -0,0 +1,47 @@
+/* Global Vue, axios */
+var tenantApi = '/api/tenants';
+
+const app = Vue.createApp({
+
+    data() {
+        return {
+            // models map (comma seperated key/values pairs)
+            tenant : new Object()
+        };
+
+
+    },
+
+    mounted(){
+        //semi colon seperated statements 
+    },
+
+    methods: {
+        //Comma seperated function declarations
+        addTenant() {
+            axios.post('/api/tenants', this.tenant)
+                .then(response => {
+                    window.location.href = 'view-property.html';
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+    },
+        //Other modules
+        mixins: []
+
+
+
+});
+
+// other component imports go here
+import { navigationMenu } from './navigation-menu.js';
+app.component('navigation-menu', navigationMenu);
+
+import { dataStore } from './data-store.js';
+app.use(dataStore);
+
+// mount this page, needs to be at the bottom of the file
+app.mount("main");
+
