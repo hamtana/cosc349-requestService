7dcff8672c0d618d41c0494bdea9a985ceb05a0a
phiha428 <phiha428@student.otago.ac.nz>
2024-08-24 15:21:22 +1200

    added Server class

---
+++ register-service/src/main/java/web/Server.java
@@ -0,0 +1,25 @@
+package web;
+
+import dao.DAOFactory;
+import dao.RequestDAO;
+import dao.TenantDAO;
+import io.jooby.Jooby;
+import io.jooby.gson.GsonModule;
+import io.jooby.StatusCode;
+import java.nio.file.Paths;
+
+public class Server extends Jooby {
+
+    private static final TenantDAO tenantDAO = DAOFactory.getTenantDAO();
+    private static final RequestDAO requestDAO = DAOFactory.getRequestDAO();
+
+    public Server(){
+        install(new GsonModule());
+        mount(new TenantModule(tenantDAO));
+
+
+    }
+
+
+}
+
