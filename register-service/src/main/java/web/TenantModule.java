67f083f6ba581781c731c3e264a0d6bc31641a03
phiha428 <phiha428@student.otago.ac.nz>
2024-08-24 12:53:05 +1200

    added Post method to tenant module, references #10

--- register-service/src/main/java/web/TenantModule.java
+++ register-service/src/main/java/web/TenantModule.java
@@ -1,12 +1,27 @@
 package web;
 
 import dao.TenantDAO;
+import helpers.ScryptHelper;
 import io.jooby.Jooby;
+import domain.Tenant;
+import io.jooby.StatusCode;
+
+import java.nio.CharBuffer;
 
 public class TenantModule extends Jooby {
 
     public TenantModule(TenantDAO dao){
 
+
+        post("api/tenants", ctx -> {
+            Tenant tenant = ctx.body().to(Tenant.class);
+            String password = tenant.getPassword();
+            CharBuffer hash = ScryptHelper.hash(password);
+            tenant.setPassword(hash.toString());
+            dao.saveTenant(tenant);
+            return ctx.send(StatusCode.CREATED);
+
+        });
 
 
     }
