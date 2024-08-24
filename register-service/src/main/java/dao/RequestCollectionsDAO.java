d5255a57522eb0039b083612d5dafdd87708ab1e
phiha428 <phiha428@student.otago.ac.nz>
2024-08-24 14:58:37 +1200

    added dummy test data to request collectionsDAO

--- register-service/src/main/java/dao/RequestCollectionsDAO.java
+++ register-service/src/main/java/dao/RequestCollectionsDAO.java
@@ -22,6 +22,31 @@ public class RequestCollectionsDAO implements RequestDAO {
     private static final HashMap<String, Request> requests = new HashMap<String, Request>();
     private static final Multimap<String, Request> requestsByTenant = HashMultimap.create();
 
+    //Test data to check if it visible on the website.
+    @SuppressWarnings("OverridableMethodCallInContructor")
+    public RequestCollectionsDAO(){
+        if(requests.isEmpty()){
+
+            //Create some dummy data
+            Request request1 = new Request("0001", "Broken Toilet", "The toilet is broken", true, null, null, false);
+            Request request2 = new Request("0002", "Broken Window", "The window is broken", false, null, null, false);
+            Request request3 = new Request("0003", "Broken Door", "The door is broken", true, null, null, false);
+            Request request4 = new Request("0004", "Broken Roof", "The roof is broken", false, null, null, false);
+
+            requests.put(request1.getId(), request1);
+            requests.put(request2.getId(), request2);
+            requests.put(request3.getId(), request3);
+            requests.put(request4.getId(), request4);
+
+            requestsByTenant.put(request1.getTenant().getId(), request1);
+            requestsByTenant.put(request2.getTenant().getId(), request2);
+            requestsByTenant.put(request3.getTenant().getId(), request3);
+            requestsByTenant.put(request4.getTenant().getId(), request4);
+
+        }
+
+    }
+
     @Override
     public List<Request> getAllRequests(){
         return new ArrayList<Request>(requests.values());
