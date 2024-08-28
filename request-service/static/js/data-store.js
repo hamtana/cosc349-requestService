7686fd1e99859b68d73fe96bfb118d093e9c4438
phiha428 <phiha428@student.otago.ac.nz>
2024-08-28 14:35:37 +1200

    added navigation-menu, closes #22

--- request-service/static/js/data-store.js
+++ request-service/static/js/data-store.js
@@ -1,28 +1,28 @@
 export const dataStore = Vuex.createStore({
 
-    //Where we declare the state of the item we want to keep track of
-    state(){
-        // Tenant that is signed in
-        tenant: null;
+    // Where we declare the state of the item we want to keep track of
+    state() {
+        return {
+            // Tenant that is signed in
+            tenant: null,
 
-        // The request that is being made/edited
-        request: null;
-
-    }
+            // The request that is being made/edited
+            request: null
+        };
+    },
 
     mutations: {
         // tenant signs in
-        signIn(state, tenant){
+        signIn(state, tenant) {
             state.tenant = tenant;
-        }
+        },
 
-        selectRequest(state, request){
+        selectRequest(state, request) {
             state.request = request;
         }
     },
 
-        // add session storage persistence
-        plugins: [window.createPersistedState({storage: window.sessionStorage})]
-
+    // add session storage persistence
+    plugins: [window.createPersistedState({ storage: window.sessionStorage })]
 
 });
