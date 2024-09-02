f8fc565dbfe23e21f8e5aa4ccc442506cc22b342
phiha428 <phiha428@student.otago.ac.nz>
2024-09-02 13:10:37 +1200

    added web classes to management service, closes #35

---
+++ management-service/src/main/java/web/Server.java
@@ -0,0 +1,40 @@
+package web;
+
+import dao.DAOFactory;
+import dao.ManagerDAO;
+import dao.PropertyDAO;
+import dao.RequestDAO;
+import dao.TenantDAO;
+import io.jooby.Jooby;
+import io.jooby.ServerOptions;
+import io.jooby.StatusCode;
+import io.jooby.gson.GsonModule;
+
+public class Server extends Jooby {
+
+    private static final ManagerDAO managerDAO = DAOFactory.getManagerDAO();
+    private static final PropertyDAO propertyDAODAO = DAOFactory.getPropertyDAO();
+
+    public Server(){
+        install(new GsonModule());
+
+        error(StatusCode.SERVER_ERROR, (ctx, cause, code) -> {
+            ctx.getRouter().getLog().error(cause.getMessage(), cause);
+            ctx.send("Server error: " + cause.getMessage());
+        });
+        
+        mount(new StaticAssetModule());
+
+
+    }
+
+    public static void main(String [] args){
+        System.out.println("\nStarting Server...");
+        new Server()
+                .setServerOptions(new ServerOptions().setPort(8081))
+                .start();
+    }
+
+
+}
+
